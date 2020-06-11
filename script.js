cfc24babbed9007ffb3a07c780cc29d9df30c398
kruzsliczblanka <kruzsliczblanka@gmail.com>
2020-06-11 15:38:59 +0200

    corrected js version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $('#fox1').text('Macrotis');
-$('#fox2').border(purple);
-$('#fox1').background('yellow');
-$('#fox2').background('yellow');
-$('#fox3').background('yellow');
+$('#fox2').css('border-color', 'purple');
+$('#fox2').css('background', 'yellow');
+$('#fox1').css('background', 'yellow');
+$('#fox3').css('background', 'yellow');
