2c3035696e9e80840149219ad12cbdcb90cc697c
kruzsliczblanka <kruzsliczblanka@gmail.com>
2020-06-11 16:24:22 +0200

    faulty

---
+++ script_directions.js
@@ -0,0 +1,20 @@
+let color = purple;
+let number = 10;
+let word: "cool";
+
+/*if $('.north') background !== "purple"
+$('.north').css('background', 'purple');
+{console.log(i)};
+*/
+
+if (number > 100) {
+    console.log("whoah, that's a big number.");
+} else { console.log("just a regular number, please.");
+};
+if (word === "cool") {
+    $('.south').text('Power of DOM');
+    console.log(south)
+} else {
+    $('.west').text('Power of DOM');
+    console.log(south)
+}
